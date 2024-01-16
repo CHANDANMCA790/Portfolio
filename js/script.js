b5f481c9f40d6d473ca8fd5a1d05758c86cbdb0a
Chandan Kumar <chandanmca790bca@gmail.com>
2024-01-17 02:01:12 +0530

    First commit

--- js/script.js
+++ js/script.js
@@ -82,3 +82,83 @@ ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { ori
 /*========== contact form==========*/
 /*<form onsubmit="return false">
 </form> */
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+function sendEmail(){
+
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Mobile Number: ${phone.value}<br> Your Message: ${message.value}<br>`;
+
+    Email.send({
+        SecureToken : "d153dbb3-ca8c-4a52-829b-ec86094bc2de",
+        To : 'chandankumar.apt1@gmail.com',
+        From : "chandankumar.apt1@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if (message == "OK"){
+            Swal.fire({
+                title: "Success!",
+                text: "Message sent successfully!",
+                icon: "success"
+              });
+        }
+      }
+    );
+}
+
+function checkInputs(){
+    const items = document.querySelectorAll(".item");
+    for (const item of items){
+        if (item.value == ""){
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != ""){
+            checkEmail();
+        }
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        });
+
+
+
+
+        item.addEventListener("keyup", () => {
+            if (item.value != ""){
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else{
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        })
+    }
+}
+function checkEmail(){
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    if (!email.value.match(emailRegex)){
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+    }
+    else{
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    checkInputs();
+    if (!fullName.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !message.classList.contains("error")){
+        sendEmail();
+        form.reset();
+        return false;
+    }
+});
